a2c05036bfa5804d4cce152b99121de89c2836ae
AliAndroidDesign <42008081+AliAndroidDesign@users.noreply.github.com>
2023-06-14 00:48:07 +0300

    logout

--- RealEstate/src/main/java/org/RealEstate/controller/TemplateController.java
+++ RealEstate/src/main/java/org/RealEstate/controller/TemplateController.java
@@ -1,5 +1,6 @@
 package org.RealEstate.controller;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,10 +9,15 @@ import java.util.ResourceBundle;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.RealEstate.dto.MenuItem;
+import org.omnifaces.util.Faces;
 
 @Named
 @SessionScoped
@@ -59,13 +65,12 @@ public class TemplateController implements Serializable {
 		m1.addChild(m2);
 		m2 = new MenuItem("", "", "village", true, "display: none;");
 		m1.addChild(m2);
-		
-		// mdi mdi-cogs 
+
+		// mdi mdi-cogs
 
 		m2 = new MenuItem("Configuration", "      mdi mdi-cogs     ", "configuration", true, "addButton");
 		m1.addChild(m2);
-		
-		
+
 		menu.add(m1);
 
 		return menu;
@@ -78,6 +83,19 @@ public class TemplateController implements Serializable {
 
 	public void setMenu(List<MenuItem> menu) {
 		this.menu = menu;
+	}
+
+	public void logout() {
+
+		try {
+			ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+			HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
+			request.logout();
+			externalContext.invalidateSession();
+			Faces.redirect("admin-login.xhtml");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	private String getValue(java.lang.String key) {
